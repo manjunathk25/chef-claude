2ceef29631d73df277174af46643d1678ceff584
manjunath25 <k.mnjunth@gmail.com>
2025-01-23 17:52:38 +0530

    main component created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Header from "./components/Header"
+import Main from "./components/Main"
 function App(){
     return(
-        <Header />
+        <>
+            <Header />
+            <Main />
+        </>
+        
     )
 }
 export default App
