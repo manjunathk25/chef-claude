ac5028a556c86483bc5153d56836f7c683135c79
manjunath25 <k.mnjunth@gmail.com>
2025-01-23 16:41:53 +0530

    header component created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import Header from "./components/Header"
 function App(){
     return(
-        <h1>App</h1>
+        <Header />
     )
 }
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header className="header">
+            <img className="chef-claude-logo" src="src/assets/chef.png" alt="chef-claude-logo" />
+            <span className="chef-claude">Chef Claude</span>
+        </header>
+    )
+}
+export default Header
