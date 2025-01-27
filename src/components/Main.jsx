dae1d34f79cf7d396c0f688cdcbbcf55dbf45c4f
manjunath25 <k.mnjunth@gmail.com>
2025-01-27 13:21:08 +0530

    creating a state variable called ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,25 @@
+import React from "react"
 function Main(){
+    /* make ingredients as state variable */ 
+    let [ingredients, setIngredients] = React.useState([])
+    function handleSubmit(event){
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+    let ingredientListElements = ingredients.map(ingredient => {
+        return <li key={ingredient}>{ingredient}</li>
+    })
     return(
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" aria-label="add ingredient" placeholder="e.g. oregano" />
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input type="text" name="ingredient" aria-label="add ingredient" placeholder="e.g. oregano" />
                 <button>Add ingredient</button>
             </form>
+            <ul className="ingredients-list">
+                {ingredientListElements}
+            </ul>
         </main>
     )
 }
