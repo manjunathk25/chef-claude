a92f5beff5b78cf1fb064c7a369df9fe23db0945
manjunath25 <k.mnjunth@gmail.com>
2025-01-29 22:25:13 +0530

    conditional rendering after adding more than 3 ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,13 +22,15 @@ function Main(){
                 <section>
                     <h2 className="ingredients-heading">Ingredients on hand:</h2>
                     <ul className="ingredients-list">{ingredientListElements}</ul>
-                    <div className="get-recipe-container">
-                        <div>
-                            <h3>Ready for a recipe?</h3>
-                            <p>Generate a recipe from your list of ingredients.</p>
+                    {ingredients.length > 3 && 
+                        <div className="get-recipe-container">
+                            <div>
+                                <h3>Ready for a recipe?</h3>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button>Get a recipe</button>
                         </div>
-                        <button>Get a recipe</button>
-                    </div>
+                    }
                 </section>
             }       
         </main>
